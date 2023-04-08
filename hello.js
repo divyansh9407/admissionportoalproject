9835b8bbfe6c423670db83434bea80c9771b67e4
divyansh9407 <adiviyanshagrawal@gmail.com>
2023-04-08 18:24:07 +0530

    hello

---
+++ hello.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+// server start
+ app.listen(port, () => {
+ console.log(`server start listening on port localhost: ${port}`)
+ //template string formate to writenodemon
+ })
+// console.log(express)
